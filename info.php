877cf09fd40fa897d1fe8d6776fc5c57fe46f5c4
unknown <arziyeva.sayyora@bk.ru>
2021-05-10 13:49:51 +0500

    Info file qo'shildi !

---
+++ info.php
@@ -0,0 +1,8 @@
+<?
+// header('Content-Type: application/json; charset=utf-8');
+header('Content-Type: text/html; charset=utf-8');
+date_default_timezone_set('Asia/Tashkent');
+function dump($what){echo '<pre>'; print_r($what); echo '</pre>';};
+
+echo "bor";
+dump($_SERVER);
